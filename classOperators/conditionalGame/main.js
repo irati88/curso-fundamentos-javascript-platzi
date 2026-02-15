d69304927974a6dd2f6f89fcc47f8c95cd4376e4
irati88 <irati.a88@gmail.com>
2026-02-15 20:12:58 +0100

    Conditional Operators: Switch

--- classOperators/conditionalGame/main.js
+++ classOperators/conditionalGame/main.js
@@ -3,6 +3,9 @@
 // Constante que nos dará un número random en un rango del 1 al 10
 const secretNumber = Math.floor(Math.random() * 10 + 1);
 
+// ** Math.floor() -> Redondea un número flotante (decimal) hacia abajo, hacia su entero más próximo
+// ** Math.random() -> Para obtener un número aleatorio
+
 // Constante con la que jugaremos y, mediante el prompt, preguntaremos al navegador
 const playerNumber = parseInt (prompt("Adivina el número secreto entre el 1 y el 10"));
 
