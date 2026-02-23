5c6f9288cf987584542394b979413de10039ed8c
irati88 <irati.a88@gmail.com>
2026-02-23 19:19:54 +0100

    For...Of loop example

--- classLoopsAndIteration/loopForOf/main.js
+++ classLoopsAndIteration/loopForOf/main.js
@@ -3,6 +3,12 @@
 // se utiliza sobre elementos de una lista, como los arrays y strings.
 
 // Sintaxis básica:
-// for (variable of objeto) {
+// for (variable + of + objeto) {
 // código a ejecutar
 // }
+
+let basket = ["manzana", "pera", "naranja", "uva"];
+
+for (fruit of basket) {
+  console.log(fruit); // Recorre el array de "basket" e imprime los elementos uno a uno
+}
