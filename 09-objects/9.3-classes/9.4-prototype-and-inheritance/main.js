b13bcd90074c80f9bbcb1f7347020eb5be92efc0
irati88 <irati.a88@gmail.com>
2026-03-09 17:32:25 +0100

    Methods and Prototype Inheritance example

--- 09-objects/9.3-classes/9.4-prototype-and-inheritance/main.js
+++ 09-objects/9.3-classes/9.4-prototype-and-inheritance/main.js
@@ -20,8 +20,9 @@ class Animal {
 
 // Clase constructora Dog
 
-class Dog extends Animal { // Añadimos "extends" y la clase padre "Animal" para poder heredar sus propiedades y métodos
-  
+class Dog extends Animal {
+  // Añadimos "extends" y la clase padre "Animal" para poder heredar sus propiedades y métodos
+
   constructor(name, type, brand) {
     super(name, type); // Añadimos super() y dentro, las propiedades de la clase padre que queramos usar
     this.brand = brand; // Añadimos nueva propiedad específica de la clase Dog
@@ -40,3 +41,20 @@ const dog1 = new Dog("Toby", "Perro", "Doberman");
 console.log(dog1); // Imprime: Dog {name: 'Toby', type: 'Perro', brand: 'Doberman'}
 dog1.run(); // Imprime el método dentro de la clase Dog: Toby corre alegremente
 dog1.displaySound(); // Imprime el método dentro de la clase Dog: El perro ladra
+
+// Crear nuevo método
+
+dog1.newMethod = function () {
+  // Aquí sólo se genera el método a la instancia dog1
+  console.log("Este es un método");
+};
+
+dog1.newMethod(); // Imprime: Este es un método
+
+// Añadir método a la clase constructora Dog
+
+Dog.prototype.secondMethod = function () {
+  console.log("Es otro nuevo método");
+};
+
+dog1.secondMethod(); // Imprime: Es otro nuevo método
