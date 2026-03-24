f771b1cfd08e553caff6c221430e084b598137b9
irati88 <irati.a88@gmail.com>
2026-03-24 17:05:57 +0100

    HTTP request types

---
+++ 13-http/main.js
@@ -0,0 +1,17 @@
+/*
+Fundamentos de HTTP: Cliente-Servidor y Métodos de Petición
+
+CLIENTE: Navegador, PC o dispositivo móvil
+SERVIDOR: Donde se hospeda el proyecto
+
+El cliente solicita o envía una petición al servidor (renderizar una página, actualizar unos datos, enviar info nueva) a través una URL
+El servidor devuelve una respuesta al cliente
+
+Verbos de HTTP:
+GET: Cuando queremos obtener info sin realizar cambios(p.e al poner una URL en el buscador y clickar Enter)
+POST: Para enviar datos nuevos al servidor (p.e al publicar en redes sociales, enviar un formulario web o subir un archivo)
+PATCH: Actualiza la información existente modificando los datos específicos (p.e cambiar la dirección de correo electrónico de un usuario)
+PUT: Actualiza la información existente reemplazándolo por datos nuevos (p.e editar un artículo en un blog)
+DELETE: Borra la información (p.e. eliminar un archivo o una publicación de un foro)
+
+*/
