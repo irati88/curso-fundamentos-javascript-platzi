0e356ca2c7cedc49b09260d426feade47b0cf705
irati88 <irati.a88@gmail.com>
2026-03-24 13:02:11 +0100

    Multiple API calls with for await...of method. Examples

--- 12-promises/12.1-async-await/main.js
+++ 12-promises/12.1-async-await/main.js
@@ -28,3 +28,30 @@ async function fetchData() {
 }
 
 fetchData();
+
+/* Realizar múltiples peticiones HTTP al interactuar con varias APIs a la vez usando for await...of, 
+combinando async y await para ejecutar promesas de manera asíncrona.
+
+*/
+
+const urls = [
+  "https://rickandmortyapi.com/api/character",
+  "https://rickandmortyapi.com/api/location",
+  "https://rickandmortyapi.com/api/episode",
+];
+
+// Creamos la función asíncrona
+async function fetchNewData() {
+  try {
+    for await (let url of urls) {
+      // Por cada url que exista en la variable urls, haz lo siguiente:
+      let response = await fetch(url); // Guarda la respuesta de cada fetch que se le haga a las urls
+      let data = await response.json(); // Convierte la respuesta en un json y guárdala en la variable data
+      console.log(data); // Imprime los datos
+    }
+  } catch (error) { // Si la petición falla, dame un error
+    console.log(error); // Imprime el error
+  }
+}
+
+
